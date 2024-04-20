2a9f74768e57115932b671c322897ddea252e14c
Maximilian Paju <maximilian.paju@gmail.com>
2024-04-20 16:23:51 +0200

    chore: adding tmux/fish/git config

---
+++ .config/fish/config.fish
@@ -0,0 +1,23 @@
+set PATH /usr/local/bin $PATH 
+
+set -g theme_color_scheme terminal-dark
+set -g fish_prompt_pwd_dir_length 1
+set -g theme_display_user yes
+set -g theme_hide_hostname no
+set -g theme_hostname always
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /opt/miniconda3/bin/conda
+    eval /opt/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/opt/miniconda3/etc/fish/conf.d/conda.fish"
+        . "/opt/miniconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/opt/miniconda3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
+echo loaded fish.config
+
